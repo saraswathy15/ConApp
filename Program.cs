5217f38a17e7b0c8ca15dea426557992f60d38af
saraswathy15 <charusrinivasan15@gmail.com>
2024-02-16 09:43:19 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+using System;
+Console.WriteLine("Welcome to Number System");
+Console.WriteLine("*** Even Numbers ***");
+NumberSystem.EvenNum();
+Console.WriteLine("*** Odd Numbers ***");
+NumberSystem.oddNum();
